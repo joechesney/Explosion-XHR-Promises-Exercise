5e9550cbeadbcd24e3066afe3606343e0e591379
joechesney <33613119+joechesney@users.noreply.github.com>
2018-01-05 11:05:05 -0600

    added some clearer variable names in formatter

--- scripts/formatter.js
+++ scripts/formatter.js
@@ -1,8 +1,8 @@
 "use strict";
 
+
 // format the data
 module.exports.formatData = function (data){
-  console.log("data!!!", data);
   // mutate the products objects and change the type to the matching string from the string json file, and add a property calledd 'category' on each product that has the value of the matching string from the categories json file
 /*
 FROM THIS:
@@ -24,20 +24,31 @@ TO THIS:
     "category": "Fireworks"
   }
 },
-*/
-// MY ARRAYS DO NOT MATCH THE SAME ORDER AS JOES
-// MY PRODUCTS ARE IN data[1] INSTEAD OF data[2] LIKE HIS
-  let revisedProds = data[1].map(function(prod){
+  */
+  // MY ARRAYS DO NOT MATCH THE SAME ORDER AS JOES
+  // MY PRODUCTS ARE IN data[1] INSTEAD OF data[2] LIKE HIS
+  let products = data[1];
+  let types = data[2];
+  let categories = data[0];
+  
+  // products is the entire products object, which is an array of objects
+  // prod is each individual index in the products array, a product object
+  let revisedProds = products.map(function(prod){
+    console.log('prod: ',prod);
+    let currentProd = Object.keys(prod)[0];
+    console.log('currentProd: ',currentProd);
+
+    // this will read the first (and only) property of each product object
     for(let prop in prod){
-      for(let i = 0; i < data[2].length; i++){
-        if(prod[prop].type === data[2][i].id){
-          prod[prop].type = data[2][i].name;
-          prod[prop].category = data[2][i].category;
+      for(let i = 0; i < types.length; i++){
+        if(prod[prop].type === types[i].id){
+          prod[prop].type = types[i].name;
+          prod[prop].category = types[i].category;
         }
       }
-      for(let k = 0; k < data[0].length; k++){
-        if(prod[prop].category === data[0][k].id){
-          prod[prop].category = data[0][k].name;
+      for(let k = 0; k < categories.length; k++){
+        if(prod[prop].category === categories[k].id){
+          prod[prop].category = categories[k].name;
         }
       }
     }
